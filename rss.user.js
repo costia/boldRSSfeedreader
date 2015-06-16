eea291f3f682b7f7ae5194eaf51ac788a241e083
costia <p.costia@gmail.com>
2015-06-16 19:00:49 +0300

    first submit

---
+++ rss.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name           Rss
+// @description    Rss
+// @include        http://feedreader.com/*
+// @version        1.0
+// ==/UserScript==
+
+
+
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+addGlobalStyle('.entry_read .entry__summary__title {font-weight: normal;}');
+addGlobalStyle('.entry__summary__title {font-weight: bold;}');
