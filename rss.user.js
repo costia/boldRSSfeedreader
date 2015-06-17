a7a15c70f20e6f3a8fcdfa2c04e44cec5152f924
costia <p.costia@gmail.com>
2015-06-18 02:06:15 +0300

    1.12
    1.12

--- rss.user.js
+++ rss.user.js
@@ -2,7 +2,7 @@
 // @name           Rss
 // @description    Rss
 // @include        http://feedreader.com/*
-// @version        1.11
+// @version        1.12
 // @namespace https://github.com/costia/boldRSSfeedreader
 // @updateURL https://github.com/costia/boldRSSfeedreader/raw/master/rss.user.js
 // @downloadURL https://github.com/costia/boldRSSfeedreader/raw/master/rss.user.js
